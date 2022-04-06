0b320602bc5e7c0b990b4efc16510161da804545
JKH-Programmer <rlgns0927@gmail.com>
2022-04-06 22:45:39 +0900

    Initial Commit

---
+++ app/build/generated/source/buildConfig/debug/fastcampus/aop/part2/viewpager2tablayout/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package fastcampus.aop.part2.viewpager2tablayout;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "fastcampus.aop.part2.viewpager2tablayout";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
